ebfde3c205b0ba8fb328a052c9566c91761a2bd8
Umesh <umeshmane575@gmail.com>
2024-03-05 11:41:59 +0530

    lesson 01 of typescript is done

---
+++ Lesson-01/build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let usernmae = "umeshmane";
+let a = 12;
+let b = '6';
+let c = 6;
+console.log(a / b);
+console.log(a * b);
---
+++ Lesson-01/src/main.ts
@@ -0,0 +1,10 @@
+let usernmae= "umeshmane"
+
+
+let a = 12 ;
+let b = '6';
+let c =  6;
+
+console.log(a/b);
+
+console.log(a * b);
