42e5d7bcd62b34a5775904805164a508d423695f
Umesh <umeshmane575@gmail.com>
2024-03-05 12:49:42 +0530

    lession two is added

---
+++ 02-DataTypes/build/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+// Data types
+// string ,  number , boolean , any , union 
+let myName = 'mane';
+let meannigofLife;
+let isLoding;
+let album; // union type
+myName = 'Umesh';
+meannigofLife = 34;
+isLoding = true;
+album = 56;
+// function
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ 02-DataTypes/src/main.ts
@@ -0,0 +1,37 @@
+
+// Data types
+
+// string ,  number , boolean , any , union 
+
+
+
+let myName : string = 'mane'
+let meannigofLife : number ;
+let isLoding :boolean ;
+
+let album : string | number ; // union type
+
+
+myName = 'Umesh'
+meannigofLife = 34 ;
+isLoding = true; 
+album = 56
+
+// function
+const sum = ( a : number  , b : string) => {
+    return a + b
+}
+
+
+let postId : string | number ; 
+let isActive : number | boolean ;
+
+let re:RegExp = /\w+/g
+
+
+ 
+
+
+
+
+
