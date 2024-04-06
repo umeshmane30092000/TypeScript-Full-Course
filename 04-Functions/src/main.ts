c01d1c6db787239386647a11178343f8e68dad55
Umesh <umeshmane575@gmail.com>
2024-04-06 15:03:43 +0530

    finction is completed

---
+++ 04-Functions/src/main.ts
@@ -0,0 +1,26 @@
+// type aliases
+
+type stringorNumber = string | number 
+
+type stringorNumberArray = (string | number)[]
+
+type Guitarist = {
+    name? : string , 
+    active : boolean ,
+    albums: stringorNumberArray
+}
+
+type UserId = stringorNumber
+
+// Literals type
+let myName: 'Dave'
+
+let userName : 'Dave' | 'john' | 'any'
+userName = "Dave"
+
+// functions
+const add = (a: number , b: number):number => {
+    return a + b
+}
+
+//type aliesase 
